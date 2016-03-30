c2328f1c784986090ae88e054fe910a75473eaa2
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-30 11:41:06 +0700

    Say 2 when put 2

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,11 @@
-function FizzBuzz() {
+function FizzBuzz(number) {
     this.say = say;
 
     function say() {
+        if (number === 2) {
+            return "2";
+        }
+
         return "1";
     }
 }
--- test/fizzbuzzSpec.js
+++ test/fizzbuzzSpec.js
@@ -3,4 +3,9 @@ describe("FizzBuzz", function(){
         var fizzbuzz = new FizzBuzz(1);
         expect(fizzbuzz.say()).toEqual("1");
     });
+
+    it("should say 2 when put 2", function(){
+        var fizzbuzz = new FizzBuzz(2);
+        expect(fizzbuzz.say()).toEqual("2");
+    });
 });
