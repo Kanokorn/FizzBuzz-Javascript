bfedfd84eb1e5273d096763ed2b07f84a90e82ed
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-30 11:55:44 +0700

    Refactor code (multiples of three print 'Fizz')

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,7 @@ function FizzBuzz(number) {
     this.say = say;
 
     function say() {
-        if (number === 3 || number === 6 || number === 9 || number === 12) {
+        if (number % 3 === 0) {
             return "Fizz";
         }
         if (number === 5 || number === 10) {
