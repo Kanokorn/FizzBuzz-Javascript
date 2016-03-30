d7597d0a97a11cb364c858b7e8a8be87b1b350db
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-30 11:45:25 +0700

    Refactor code

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,18 +2,10 @@ function FizzBuzz(number) {
     this.say = say;
 
     function say() {
-        if (number === 2) {
-            return "2";
-        }
-
         if (number === 3) {
             return "Fizz";
         }
 
-        if (number === 4) {
-            return "4";
-        }
-
-        return "1";
+        return number.toString();
     }
 }
